bd207b052ec37b5bcae2e0ede66c3aa4990c088f
QuerMann <quermanmarkus@gmail.com>
2021-06-12 04:05:29 +0300

    Таймаут между проверками выбираетсся рандомизатором

--- cmsValidate.py
+++ cmsValidate.py
@@ -7,46 +7,38 @@ import time
 from datetime import date
 import config
 import logManager
+import random
 
 def main():
     logManager.cmsLogger('Запущен модуль валидации экранов')
-
-
     # Запись статуса вхождения пользователя
     d = date.today()
     userStateFile = open('{}userState.txt'.format(config.tempPath, str(d)), 'w')
     userStateFile.write('1')
     userStateFile.close()
-
-
-    # chanel_sum_1 = []
-    chanelIntencArr = []
-    # chanel_sum_2 = []
+    chanelSumArr = []
 
     if config.screenNum == 1:
         while True:
-            while len(chanelIntencArr) < 2 :
+            while len(chanelSumArr) < 2 :
                 # Функциональная часть снимка экрана и подсчета интнсивности каналов
-                image = pyautogui.screenshot(region=(config.regiondict[0])) # Снимок экрана
-                img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)      # передача изображения на анализ каналов
-                intensity = img.sum(axis=2)                                 # посмотреть что делает эта строка
-                chanelsum = np.sum(intensity)                               # Сумма интенсивности по каналам
-
-                if len(chanelIntencArr) < 2 :
-                    chanelIntencArr.append(chanelsum)
-
+                screenShot = pyautogui.screenshot(region=(config.regiondict[0])) # Снимок экрана
+                chanelIntens = cv2.cvtColor(np.array(screenShot), cv2.COLOR_RGB2BGR)      # передача изображения на анализ каналов
+                commonIntens = chanelIntens.sum(axis=2)                                 # посмотреть что делает эта строка
+                chanelSum = np.sum(commonIntens)                               # Сумма интенсивности по каналам
+                if len(chanelSumArr) < 2 :
+                    chanelSumArr.append(chanelSum)
             # Часть проверок значений, записи состояний
-            if len(chanelIntencArr) == 2:
+            if len(chanelSumArr) == 2:
                 f = open('{}screenState.txt'.format(config.tempPath), 'w') # Пишу состояние экрана
-                if (chanelIntencArr[0] == chanelIntencArr[1]):
+                if (chanelSumArr[0] == chanelSumArr[1]):
                     f.write('1')
                 else:
                     f.write('0')
                 f.close()
-                del chanelIntencArr[0] # Удаляю из словаря запись с индексом 0
+                del chanelSumArr[0] # Удаляю из словаря запись с индексом 0
 
-            # добавить рандомизатор
-            time.sleep(43)
+            time.sleep(random.randint(30, 90))
     else:
         pass
 
