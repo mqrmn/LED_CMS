48409ab87a19e7bae78c2c7f74f74e080f1ea214
QuerMann <quermanmarkus@gmail.com>
2021-06-29 02:42:44 +0500

    Настроено логирование и приоретизация

--- App/FileManager.py
+++ App/FileManager.py
@@ -69,8 +69,6 @@ class System:
             logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Файл удален ' + file)
 
 
-
-
     def CMSUpgrade(self):
 
         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Called')
@@ -92,90 +90,69 @@ class System:
                 file.close()
                 versions['CURRENT'] = currentV
 
-            # Чтение данных из GLOBAL
-            if os.path.exists(Config.globalCmsRenew + 'PACKAGE.ver'):
-                file = open(Config.globalCmsRenew + 'PACKAGE.ver', 'r')
-                globalV = file.read()
-                file.close()
+            priorities['GLOBAL'], versions['GLOBAL'] = self.CheckCMSUpdates(Config.globalCmsRenew, 'GLOBAL')
+            print(priorities['GLOBAL'], versions['GLOBAL'])
+            priorities['GROUP'], versions['GROUP'] = self.CheckCMSUpdates(Config.groupCmsRenew, 'GROUP')
+            print(priorities['GROUP'], versions['GROUP'])
+            priorities['LOCAL'], versions['LOCAL'] = self.CheckCMSUpdates(Config.localCmsRenew, 'LOCAL')
+            print(priorities['LOCAL'], versions['LOCAL'])
 
-                versions['GLOBAL'] = globalV
-                file = open(Config.globalCmsRenew + 'UPGRADE.key', 'r')
-                globalKey = file.read()
-                file.close()
-
-                globalCMS = upgradeKeys[globalKey]
-
-                if globalKey == 'LOCK':
-                    globalCMS += 10
-                else:
-                    globalCMS += 30
-            priorities['GLOBAL'] = globalCMS
-
-            # Чтение данных из GROUP
-            if os.path.exists(Config.groupCmsRenew + 'PACKAGE.ver'):
-                file = open(Config.groupCmsRenew + 'PACKAGE.ver', 'r')
-                groupV = file.read()
-                file.close()
-
-                versions['GROUP'] = groupV
-                file = open(Config.groupCmsRenew + 'UPGRADE.key', 'r')
-                groupKey = file.read()
-                file.close()
-
-                groupCMS = upgradeKeys[groupKey]
-
-                if groupKey == 'LOCK':
-                    groupCMS += 20
-                else:
-                    groupCMS += 20
-            priorities['GROUP'] = groupCMS
-
-            # Чтение данных из LOCAL
-            if os.path.exists(Config.localCmsRenew + 'PACKAGE.ver'):
-                file = open(Config.localCmsRenew + 'PACKAGE.ver', 'r')
-                localV = file.read()
-                file.close()
-
-                versions['LOCAL'] = localV
-                file = open(Config.localCmsRenew + 'UPGRADE.key', 'r')
-                localKey = file.read()
-                file.close()
-
-                localCMS = upgradeKeys[localKey]
-
-                if localKey == 'LOCK':
-                    localCMS += 30
-                else:
-                    localCMS += 10
-            priorities['LOCAL'] = localCMS
 
             maxPriority = sorted(priorities, key=priorities.__getitem__)[-1]
             logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Определен приоритет каталога обновлений: ' + maxPriority)
-            currentVArr = versions['CURRENT'].split('.')
-            newtVArr = versions[maxPriority].split('.')
+            print(priorities[maxPriority])
+            if priorities[maxPriority] > 200:
+                currentVArr = versions['CURRENT'].split('.')
+                newtVArr = versions[maxPriority].split('.')
 
-            # Сравнение версий
-            stopCheck = 0
-            if currentVArr[0] == newtVArr[0]:
-                if currentVArr[1] == newtVArr[1]:
-                    if currentVArr[2] == newtVArr[2]:
-                        logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Не обнаружено обновлений')
-                    elif int(currentVArr[2]) < int(newtVArr[2]):
+                # Сравнение версий
+                stopCheck = 0
+                if currentVArr[0] == newtVArr[0]:
+                    if currentVArr[1] == newtVArr[1]:
+                        if currentVArr[2] == newtVArr[2]:
+                            logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Не обнаружено обновлений')
+                        elif int(currentVArr[2]) < int(newtVArr[2]):
+                            logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Обнаружено обновление ' + versions[maxPriority])
+                            self.CurrentCMSArch(currentV)
+                            self.RenewCMSFiles(Config.globalCmsRenew)
+                            stopCheck = 1
+                    elif int(currentVArr[1]) < int(newtVArr[1]) and stopCheck != 1:
                         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Обнаружено обновление ' + versions[maxPriority])
                         self.CurrentCMSArch(currentV)
-                        self.RenewCMSFiles(Config.globalCmsRenew)
-                        stopCheck = 1
-                elif int(currentVArr[1]) < int(newtVArr[1]) and stopCheck != 1:
+                        self.RenewCMSFiles(Config.groupCmsRenew)
+                elif  int(currentVArr[0]) < int(newtVArr[0]) and stopCheck != 1:
                     logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Обнаружено обновление ' + versions[maxPriority])
                     self.CurrentCMSArch(currentV)
-                    self.RenewCMSFiles(Config.groupCmsRenew)
-            elif  int(currentVArr[0]) < int(newtVArr[0]) and stopCheck != 1:
-                logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Обнаружено обновление ' + versions[maxPriority])
-                self.CurrentCMSArch(currentV)
-                self.RenewCMSFiles(Config.globalCmsRenew)
+                    self.RenewCMSFiles(Config.globalCmsRenew)
+            else:
+                logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Приоритет недостаточен. Отмена обновления.')
         else:
             pass
         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Closed')
+
+    def CheckCMSUpdates(self, path, type):
+
+        upgradeScoreKey = {'IGNORE': 0, 'FREE': 200, 'FORCE': 300, 'LOCK': 400}
+        upgradeScoreType = {'GLOBAL': 30, 'GROUP': 20, 'LOCAL': 10}
+        upgradeScoreTypeLock = {'GLOBAL': 10, 'GROUP': 20, 'LOCAL': 30}
+
+        if os.path.exists(path + 'PACKAGE.ver'):
+            file = open(Config.globalCmsRenew + 'PACKAGE.ver', 'r')
+            version = file.read()
+            file.close()
+
+            file = open(path + 'UPGRADE.key', 'r')
+            key = file.read()
+            file.close()
+
+            score = upgradeScoreKey[key]
+            if key == 'LOCK':
+                score += upgradeScoreTypeLock[type]
+            else:
+                score += upgradeScoreType[type]
+
+            return score, version
+
 
 
     def CurrentCMSArch(self, version):
@@ -183,15 +160,15 @@ class System:
         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Called')
 
         if  not os.path.exists(Config.CMSArchPath + str(version)):
-            shutil.copytree(os.getcwd(), Config.CMSArchPath + str(version))
+            #shutil.copytree(os.getcwd(), Config.CMSArchPath + str(version))
             logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Текщий пакет заархивирован')
 
         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Closed')
 
     def RenewCMSFiles(self, path):
         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Called')
-        shutil.rmtree(os.path.dirname(__file__))
-        shutil.copytree(path, os.path.dirname(__file__))
+        # shutil.rmtree(os.path.dirname(__file__))
+        # shutil.copytree(path, os.path.dirname(__file__))
 
         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Обновление выполнено')
         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'Closed')
