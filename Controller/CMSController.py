1367e5c42ac893ff0f6add2b071a4657ca30ba9f
QuerMann <quermanmarkus@gmail.com>
2021-08-21 23:53:15 +0500

    Получение флага управления перезагрузкой контроллером

--- Controller/CMSController.py
+++ Controller/CMSController.py
@@ -17,7 +17,7 @@ from inspect import currentframe, getframeinfo
 sys.path.append("C:\\MOBILE\\Local\\CMS")
 
 from App.Config import Config
-from App import API, File, Action, LogManager, Controller, Database
+from App import LogManager, API, File, Comm, Resource, Action, Controller, Handler, Database
 
 logging = LogManager._Log_Manager_()
 logHandler = logging.InitModule(os.path.splitext(os.path.basename(__file__))[0])
@@ -68,49 +68,76 @@ class AppServerSvc(win32serviceutil.ServiceFramework):
         # Создание экзепляров классов
         C_Win = API.Win()
         C_FileMan = File.Manager()
-        C_Action = Action.System()
+        C_ActionSys = Action.System()
+        C_ActionInit = Action.Init()
         C_Control = Controller.CMS()
-
+        C_Network = Comm.Socket()
+        C_Handler = Handler.Queue()
         # Создание очередей
         Q_Internal = queue.Queue()
+        Q_FromCore = queue.Queue()
+        Q_Manage = queue.Queue()
 
         # Инициализация потоков
         T_Updater = threading.Thread(target=C_Control.CMSUpdater, args=(Q_Internal,))
+        T_Server = threading.Thread(target=C_Network.Server,
+                                    args=(Config.localhost, Config.CMSControllertPort, Q_FromCore))
+        # TQ_FromCore = threading.Thread(target=C_Handler.FromCore, args=(Q_FromCore, Q_Manage))
 
         # Запуск потоков
         T_Updater.start()
+        T_Server.start()
+        # TQ_FromCore.start()
 
         # Цикл
         # --------------------------------------------------------------------
         checkTime = datetime.datetime.now()
+        table = Database.Tables()
+
+        FLAG = C_ActionInit.CheckLastShutdown(Q_Manage)
+
         while True:
+            if Q_Manage.empty() == False:
+                FLAG = Q_Manage.get()[Resource.root[3]]
+                logging.CMSLogger(logHandler, getframeinfo(currentframe())[2],
+                                  'Флаг управления перезагрузкой: {}'.format(FLAG))
+                print('FLAG', FLAG)
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as Socket:
                 try:
                     Socket.connect((Config.localhost, Config.CMSCoreInternalPort))
                     checkTime = datetime.datetime.now()
                     logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'CMS Runned', )
                 except:
-
                     if Q_Internal.empty() == False:
                         if Q_Internal.get() == True:
                             logging.CMSLogger(logHandler, getframeinfo(currentframe())[2],
                                               'CMS Stopped for upgrade', )
                             break
-
                         else:
                             pass
                     else:
                         logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'CMS Stopped', )
-                        if ((checkTime - datetime.datetime.now()).seconds >= 300):
-                            table = Database.Tables()
-                            table.SelfInitShutdown.create(trigger='CMSController',
-                                                          key='reboot',
-                                                          datetime=datetime.datetime.now(), )
-                            logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'REBOOT')
-                            C_Action.Reboot()
-                            break
+                        if FLAG > 0:
+                            if FLAG > 1:
+                                C_ActionSys.Reboot()
 
-            time.sleep(30)
+                                logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'reboot')
+                                break
+                            else:
+
+                                lastReboot = table.SelfInitShutdown().select().order_by(
+                                    table.SelfInitShutdown.id.desc()).get()
+                                if (datetime.datetime.now() - lastReboot.datetime).seconds <= 300:
+                                    C_ActionSys.Reboot()
+
+                                    logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'reboot')
+                                    break
+                                else:
+                                    logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'rebootAccessDenied')
+                        else:
+                            logging.CMSLogger(logHandler, getframeinfo(currentframe())[2], 'rebootAccessDenied')
+
+            time.sleep(10)
 
         # Граница цикла
         #----------------------------------------------------------------------
