1e648b5ab29e61302159a35d99e258c211a875ed
QuerMann <quermanmarkus@gmail.com>
2021-08-26 21:18:48 +0500

    CMSController.py, перенос проверки CMSService в поток

--- TEST/EventLog.py
+++ TEST/EventLog.py
@@ -1,35 +1,16 @@
 
-from App import Log
-
 import win32evtlog
-
 import win32evtlogutil
-
-import win32security
-
 import win32con
-
 import winerror
-
 import time
 
 import re
 
-import string
-
-import sys
-
-import traceback
 import datetime
 
 def TEST():
-    ####Main program
-
-    # initialize variables
     flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
-
-    # This dict converts the event type into a human readable form
-
     evt_dict = {win32con.EVENTLOG_INFORMATION_TYPE: 'EVENTLOG_INFORMATION_TYPE',
                 win32con.EVENTLOG_WARNING_TYPE: 'EVENTLOG_WARNING_TYPE',
                 win32con.EVENTLOG_ERROR_TYPE: 'EVENTLOG_ERROR_TYPE'}
@@ -39,8 +20,6 @@ def TEST():
     # logtype = 'Application'
     begin_sec = time.time()
     begin_time = time.strftime('%H:%M:%S  ', time.localtime(begin_sec))
-
-    # open event log
 
     hand = win32evtlog.OpenEventLog(computer, logtype)
     print(logtype, ' events found in the last 8 hours since:', begin_time)
@@ -66,18 +45,10 @@ def TEST():
             events = win32evtlog.ReadEventLog(hand, flags, 0)
             for ev_obj in events:
 
-                # check if the event is recent enough
-
-                # only want data from last 8hrs
-
                 the_time = ev_obj.TimeGenerated
-
-
                 if (datetime.datetime.now() - the_time).days <= 3:
                     # if str(ev_obj.SourceName) in list_2[3]:
                     if str(ev_obj.SourceName):
-
-
                         print()
 
                         cat = str(ev_obj.EventCategory)
@@ -92,8 +63,6 @@ def TEST():
         except:
             print('EXC')
     win32evtlog.CloseEventLog(hand)
-
-
 
 if __name__ == '__main__':
     TEST()
