52d436c041ef1d07dd6f2c4f4680a9c6fcda9ddf
QuerMann <quermanmarkus@gmail.com>
2021-08-29 21:22:41 +0500

    Незначительные изменения

--- App/Act.py
+++ App/Act.py
@@ -274,7 +274,7 @@ class SysInit(Files):
                                                       'Код события: {}, \n' \
                                                       'Описание: {} '.format(the_time.Format(), evt_type, src, evt_id,
                                                                              msg)
-                                    break
+                                        break
 
 
 
